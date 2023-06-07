1b35a015ac412a6a9884eaa81baad0ce7dfafaf1
Ryan Gourlie <52299431+fungal-spore@users.noreply.github.com>
2023-06-07 11:05:46 -0600

    Create busco.sh

---
+++ Assembly_Annotation/busco.sh
@@ -0,0 +1,6 @@
+/home/AAFC-AAC/gourlier/mambaforge/envs/fungap3/bin/busco \
+ --mode proteins --out busco \
+ --in /isilon/projects/J-003002_Ptr_genomics/predic/SW21-5_hicanu/fungap_out/fungap_out_prot.faa \
+ --out_path /isilon/projects/J-003002_Ptr_genomics/predic/SW21-5_hicanu/fungap_out \
+ --lineage_dataset /isilon/projects/J-003002_Ptr_genomics/predic/busco_downloads/lineages/pleosporales_odb10 \
+ --force
