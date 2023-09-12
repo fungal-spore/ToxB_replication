36765f3c994197407f239d71234c14879d7b7e16
Ryan Gourlie <52299431+fungal-spore@users.noreply.github.com>
2023-09-12 11:51:12 -0600

    Update master.sh

--- inserts/master.sh
+++ inserts/master.sh
@@ -5,8 +5,12 @@ python parse-insert-hits.py $1 $2
 
 #fiter parsed
 python parse-out.py
+cat out.count >> insert_counts.tsv
 
 #extract outs
 bedtools getfasta -fi $3 -bed out.bed -fo $2.$3.asmb
 
-rm output.tsv out.bed
+#For testing, checks if the number of hits from the parse file matches number of extracted sequences
+#grep -c '>' $2.$3.asmb >> insert_counts.tsv
+
+rm output.tsv out.bed out.count
