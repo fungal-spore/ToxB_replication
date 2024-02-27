df47278c4f376faae01de99540d8ad919d18d14f
Ryan Gourlie <52299431+fungal-spore@users.noreply.github.com>
2024-02-27 13:57:04 -0700

    Update dotplotly.sh

--- Alignments/dotplotly.sh
+++ Alignments/dotplotly.sh
@@ -1,2 +1,5 @@
+#check dotplotly github for the R script
+#mamba activate R
+#check R script and make sure you have necessary repos installed
 ./pafCoordsDotPlotly.R -i $1 -o out -s -t -m 25 -q 25 -l
 mv out.png $1.png
