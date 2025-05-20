126a8a306ef1b2a390e01d011a1ffef17bb713d8
Ryan Gourlie <52299431+fungal-spore@users.noreply.github.com>
2025-05-20 11:32:59 -0600

    Create intersect-all.sh

---
+++ inserts/intersect-all.sh
@@ -0,0 +1,25 @@
+# mamba activate bedtools
+for i in $(ls *.blastout); do bash parse_90-output.sh $1 251"; done
+
+bedtools intersect -wb -a Tptr3-1*blasout.bed -b /home/AGR.GC.CA/gourlier/earlGrey/Tptr3-1_hicanu/Tptr3-1_EarlGrey/Tptr3-1_summaryFiles/Tptr3-1.filteredRepeats.bed > Tptr3-1_SJ-TE-intersects.bed
+bedtools intersect -wb -a 107224*blasout.bed -b /home/AGR.GC.CA/gourlier/earlGrey/107224_hicanu/107224_EarlGrey/107224_summaryFiles/107224.filteredRepeats.bed > 107224_SJ-TE-intersects.bed
+bedtools intersect -wb -a 92-171*blasout.bed -b /home/AGR.GC.CA/gourlier/earlGrey/92-171_hicanu/92-171_EarlGrey/92-171_summaryFiles/92-171.filteredRepeats.bed > 92-171_SJ-TE-intersects.bed
+bedtools intersect -wb -a Den17*blasout.bed -b /home/AGR.GC.CA/gourlier/earlGrey/Den17_hicanu/Den17_EarlGrey/Den17_summaryFiles/Den17.filteredRepeats.bed > Den17_SJ-TE-intersects.bed
+bedtools intersect -wb -a G9-7*blasout.bed -b /home/AGR.GC.CA/gourlier/earlGrey/G9-7_hicanu/G9-7_EarlGrey/G9-7_summaryFiles/G9-7.filteredRepeats.bed > G9-7_SJ-TE-intersects.bed
+bedtools intersect -wb -a SC29-1*blasout.bed -b /home/AGR.GC.CA/gourlier/earlGrey/SC29-1_canu/SC29-1_EarlGrey/SC29-1_summaryFiles/SC29-1.filteredRepeats.bed > SC29-1_SJ-TE-intersects.bed
+bedtools intersect -wb -a T128-1*blasout.bed -b /home/AGR.GC.CA/gourlier/earlGrey/T128-1_canu/T128-1_EarlGrey/T128-1_summaryFiles/T128-1.filteredRepeats.bed > T128-1_SJ-TE-intersects.bed
+bedtools intersect -wb -a 90-2*blasout.bed -b /home/AGR.GC.CA/gourlier/earlGrey/90-2_canu/90-2_EarlGrey/90-2_summaryFiles/90-2.filteredRepeats.bed > 90-2_SJ-TE-intersects.bed
+bedtools intersect -wb -a T103-1*blasout.bed -b /home/AGR.GC.CA/gourlier/earlGrey/T103-1_canu/T103-1_EarlGrey/T103-1_summaryFiles/T103-1.filteredRepeats.bed > T103-1_SJ-TE-intersects.bed
+bedtools intersect -wb -a AlgH1*blasout.bed -b /home/AGR.GC.CA/gourlier/earlGrey/AlgH1_hicanu/AlgH1_EarlGrey/AlgH1_summaryFiles/AlgH1.filteredRepeats.bed > AlgH1_SJ-TE-intersects.bed
+bedtools intersect -wb -a I-34-1*blasout.bed -b /home/AGR.GC.CA/gourlier/earlGrey/I-34-1_hicanu/I-34-1_EarlGrey/I-34-1_summaryFiles/I-34-1.filteredRepeats.bed > I-34-1_SJ-TE-intersects.bed
+bedtools intersect -wb -a 331-2*blasout.bed -b /home/AGR.GC.CA/gourlier/earlGrey/331-2_hicanu/331-2_EarlGrey/331-2_summaryFiles/331-2.filteredRepeats.bed > 331-2_SJ-TE-intersects.bed
+bedtools intersect -wb -a D308*blasout.bed -b /home/AGR.GC.CA/gourlier/earlGrey/D308_flye/D308_EarlGrey/D308_summaryFiles/D308.filteredRepeats.bed > D308_SJ-TE-intersects.bed
+bedtools intersect -wb -a SC22-2*blasout.bed -b /home/AGR.GC.CA/gourlier/earlGrey/SC22-2_hicanu/SC22-2_EarlGrey/SC22-2_summaryFiles/SC22-2.filteredRepeats.bed > SC22-2_SJ-TE-intersects.bed
+bedtools intersect -wb -a SC29-8*blasout.bed -b /home/AGR.GC.CA/gourlier/earlGrey/SC29-8_hicanu/SC29-8_EarlGrey/SC29-8_summaryFiles/SC29-8.filteredRepeats.bed > SC29-8_SJ-TE-intersects.bed
+bedtools intersect -wb -a SW21-5*blasout.bed -b /home/AGR.GC.CA/gourlier/earlGrey/SW21-5_hicanu/SW21-5_EarlGrey/SW21-5_summaryFiles/SW21-5.filteredRepeats.bed > SW21-5_SJ-TE-intersects.bed
+bedtools intersect -wb -a I-33-16*blasout.bed -b /home/AGR.GC.CA/gourlier/earlGrey/I-33-16_hicanu/I-33-16_EarlGrey/I-33-16_summaryFiles/I-33-16.filteredRepeats.bed > I-33-16_SJ-TE-intersects.bed
+bedtools intersect -wb -a K11*blasout.bed -b /home/AGR.GC.CA/gourlier/earlGrey/K11_hicanu/K11_EarlGrey/K11_summaryFiles/K11.filteredRepeats.bed > K11_SJ-TE-intersects.bed
+bedtools intersect -wb -a K9*blasout.bed -b /home/AGR.GC.CA/gourlier/earlGrey/K9_hicanu/K9_EarlGrey/K9_summaryFiles/K9.filteredRepeats.bed > K9_SJ-TE-intersects.bed
+bedtools intersect -wb -a K6*blasout.bed -b /home/AGR.GC.CA/gourlier/earlGrey/K6_hicanu/K6_EarlGrey/K6_summaryFiles/K6.filteredRepeats.bed > K6_SJ-TE-intersects.bed
+bedtools intersect -wb -a T126-1*blasout.bed -b /home/AGR.GC.CA/gourlier/earlGrey/T126-1_hicanu/T126-1_EarlGrey/T126-1_summaryFiles/T126-1.filteredRepeats.bed > T126-1_SJ-TE-intersects.bed
+bedtools intersect -wb -a I-73-1*blasout.bed -b /home/AGR.GC.CA/gourlier/earlGrey/I-73-1_flye/I-73-1_EarlGrey/I-73-1_summaryFiles/I-73-1.filteredRepeats.bed > I-73-1_SJ-TE-intersects.bed
