b3beda259e2c23057d7ca4326ad08ad187435692
Ryan Gourlie <52299431+fungal-spore@users.noreply.github.com>
2025-11-10 11:37:14 -0700

    Update minimap_gggenomes.sh

--- Alignments/minimap_gggenomes.sh
+++ Alignments/minimap_gggenomes.sh
@@ -1,1 +1,5 @@
+# -X skips self and dual mapping
+# -N reatin secondary alignments
+# -p minimum secondary-to-primary score ratio
+# -c ouptu cigar to PAF
 minimap2 -X -N 5 -p 0.5 -c gggenomes_ToxB.fasta gggenomes_ToxB.fasta > gggenomes_ToxB.paf
